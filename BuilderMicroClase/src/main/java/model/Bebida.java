0f47730ba59a2082c9619ec6fd4bcc4048e83b87
Moundz <132116087+Moundz@users.noreply.github.com>
2025-09-18 09:55:08 -0500

    Add files via upload

---
+++ BuilderMicroClase/src/main/java/model/Bebida.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ESTACION
+ */
+public class Bebida {
+    
+    private String base;
+    private String sabor;
+    private boolean helado;
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public void setHelado(boolean helado) {
+        this.helado = helado;
+    }
+
+    @Override
+    public String toString() {
+        return "Bebida{" + "Base: " + base + ", Sabor: " + sabor + ", Helado: " + helado + '}';
+    }
+
+   
+    
+}
